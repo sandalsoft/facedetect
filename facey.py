d4f6162ab770da411b836c3f2caba7a75f6a6a09
Eric Nelson <eric@sandalsoft.com>
2013-07-03 10:11:09 -0500

    - Added user switching for non-current user - fixed determining user with most confidence - Added notification center

--- facey.py
+++ facey.py
@@ -20,6 +20,7 @@ client = FaceClient('33f9c5446fef45e6b16acc3ce7dab0d8', '6dd4226bbf4c4dd18dc3efd
 
 def detect_face():
 	detect = client.faces_detect(file=local_photo, aggressive=True)
+	pp.pprint(detect)
 	face_tags = detect['photos'][0]['tags'][0]
 	if face_tags:
 		attributes = face_tags['attributes']
@@ -53,23 +54,55 @@ def recognize_face():
 	recog = client.faces_recognize(uids='all', file=local_photo, aggressive=True, namespace='ss.test')
 	tags = recog['photos'][0]['tags']
 
-	pp.pprint(tags);
+	# pp.pprint(tags);
 	
 	if (tags):
-		uids = tags[0]['uids'][0]['confidence']
-		pp.pprint(uids)
+		uids = tags[0]['uids']#['confidence']
+
+		## Get highest confidence uid tag
+		highest_confidence_uid = max(uids, key=lambda x:x['confidence'])
+		if highest_confidence_uid['confidence'] < 60 :
+			print "Unable to determine who this is"
+			print uids
+			exit()
+		else:
+			print "we are confident: " + str(highest_confidence_uid['confidence'])
+
+		print highest_confidence_uid['uid']
+		if highest_confidence_uid['uid'] == eric_id:
+			# print "hi is eric"
+			send_greeting("Eric", 501)
+		if highest_confidence_uid['uid'] == julie_id:
+			send_greeting("Julie", 502)
+			
+		# pp.pprint(uids)
 	else:
 		print "Face not recognized"
 
+def send_greeting(name, uid):
+	current_uid = os.getuid()
+	# print current_uid
+	if uid == current_uid:
+		notify("Welcome back " + name)
+	else:
+		# notify("Please login " + name)
+		switch_user(str(uid))
 
-def notify(message=""):
+def switch_user(uid):
+	exec_cmd = "/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession"
+	print exec_cmd
+	call([exec_cmd, "-switchToUserID", uid])
+
+
+def notify(message):
+	print message
 	exec_cmd = "terminal-notifier"
 	call([exec_cmd, "-message", message, "-title", "Human Detected"])
 
 
 def main():
-	detect_face()
-	# recognize_face()
+	# detect_face()
+	recognize_face()
 
 if  __name__ =='__main__':main()
 
